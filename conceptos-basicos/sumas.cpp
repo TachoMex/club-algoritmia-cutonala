ce74dab089b9e0eb951d885415152547cb394cf6
Gilberto Vargas <tachoguitar@gmail.com>
2018-05-06 12:32:52 -0500

    [BK] Add examples for reading until EOF

---
+++ conceptos-basicos/sumas.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  cin.tie(0);
+  cin.sync_with_stdio(0);
+  int a, b;
+
+  while(cin >> a >> b){
+    cout << a + b <<  '\n';
+  }
+
+  return 0;
+}
